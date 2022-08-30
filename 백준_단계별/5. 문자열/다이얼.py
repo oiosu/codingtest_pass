b6adfd393745c1e2ee865a1bda60f74e3f0415cf
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2022-08-31 03:13:24 +0900

    Add files via upload

---
+++ 백준_단계별/5. 문자열/다이얼.py
@@ -0,0 +1,8 @@
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+a = input()
+ret = 0
+for j in range(len(a)):
+    for i in dial:
+        if a[j] in i:
+            ret += dial.index(i)+3
+print(ret)
