008ef0859db17e1f0b6489c0b68af21acf06efe4
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2022-09-21 10:08:34 +0900

    Add files via upload

---
+++ BAJ_백준/백준_분류_그리디/회의실배경.py
@@ -0,0 +1,19 @@
+N = int(input())
+time = []
+
+for _ in range(N):
+  start, end = map(int, input().split())
+  time.append([start, end])
+
+time = sorted(time, key=lambda a: a[0]) # 시작 시간을 기준으로 오름차순
+time = sorted(time, key=lambda a: a[1]) # 끝나는 시간을 기준으로 다시 오름차순
+
+last = 0 # 회의의 마지막 시간을 저장할 변수
+conut = 0 # 회의 개수를 저장할 변수
+
+for i, j in time:
+  if i >= last: # 시작시간이 회의의 마지막 시간보다 크거나 같을경우
+    conut += 1
+    last = j
+
+print(conut)
