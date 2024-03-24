224a89cb2a5d66889d7463c44d9ffc11d9c85c69
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-03-24 11:43:22 +0900

    PCCE 01

---
+++ 프로그래머스/PCCE 기출문제/01_출력.py
@@ -0,0 +1,16 @@
+# 문제 설명
+# 주어진 초기 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+# 출력 예시
+# Spring is beginning
+# 13
+# 310
+
+
+string_msg = "Spring is beginning"
+int_val = 3
+string_val = "3"
+
+print(string_msg)
+print(int_val + 10)
+print(string_val + "10")
